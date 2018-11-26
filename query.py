13a30b3988575be517992efffc1e2209ab51aae3
Jé <38817318+JessicaCarmona@users.noreply.github.com>
2018-11-26 15:02:08 -0400

    Update query.py

--- query.py
+++ query.py
@@ -33,8 +33,9 @@ Q3 = """WITH percent AS (
        SELECT date, prc_error FROM percent WHERE prc_error>1;"""
 
 
-# Code to xecute a queries, openning and closing the connection
+# Code to execute a queries, openning and closing the connection
 def query(query_code):
+    """Executes PSQL Codes"""
     db = psycopg2.connect(database="news")
     c = db.cursor()
     c.execute(query_code)
@@ -45,12 +46,14 @@ def query(query_code):
 
 # Codes to print the report with each query results
 def Heading():
+    """Prints Report Heading""""
     Heading = "Newsdata Results"
-    print("\n----------------------------------------------------------------\n")    
+    print("\n----------------------------------------------------------\n")
     print("\t\t\t" + Heading + "\n")
 
 
 def Top_3_Articles():
+    """Prints a list of the Top Three Articles and their total views"""
     Top_3_Articles = query(Q1)
     print("\n --> The Top 3 Articles of All Times\n")
 
@@ -59,6 +62,7 @@ def Top_3_Articles():
 
 
 def Most_Popular_Authors():
+    """Prints a list of the names of Popular Authors with their total views"""
     Most_Popular_Authros = query(Q2)
     print("\n --> Most Popular Authors of All Times\n")
 
@@ -67,12 +71,13 @@ def Most_Popular_Authors():
 
 
 def Prc_Error():
+    """Prints date and percent of requests with error when greater than 1%"""
     Prc_Error = query(Q3)
     print("\n --> Days with more than 1% percent of requests with error\n")
 
     for date, prc_error in Prc_Error:
         print("\t* {}:  {} % error\n\n".format(date, prc_error))
-        print("----------------------------------------------------------------\n")
+        print("----------------------------------------------------------\n")
 
 
 if __name__ == '__main__':
@@ -80,47 +85,3 @@ if __name__ == '__main__':
     Top_3_Articles()
     Most_Popular_Authors()
     Prc_Error()
-
-
-# VIEW TABLES:
-
-"""
-CREATE VIEW Table1 AS
-SELECT title,
-    name
-FROM Articles JOIN Authors ON articles.author = authors.id;
-
-
-CREATE VIEW Table2 AS
-SELECT status,
-    to_char(time, 'FMMon, DD YYYY') "date",
-    substr(path, 10) "slug"
-FROM log;
-
-
-CREATE VIEW Table3 AS
-SELECT title,
-    name,
-    status,
-    date
-FROM Table2 JOIN Table1 ON Table2.slug = Table1.slug;
-
-
-CREATE VIEW Table4 AS
-WITH total_status AS (
-        SELECT date,
-            count(*) AS total
-            FROM Table2
-            GROUP BY date),
-    error_status AS (
-        SELECT date,
-            count(*) AS errors
-        FROM Table2
-        WHERE status !='200 OK'
-        GROUP BY date)
-SELECT total_status.date,
-    total,
-    errors
-FROM total_status JOIN error_status ON total_status.date = error_status.date
-ORDER BY date;
-"""
